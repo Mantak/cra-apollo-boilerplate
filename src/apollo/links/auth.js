63319824628b556b2018c2d3096309cf29d344e8
mantak <mantak@foxmail.com>
2019-04-17 17:49:13 +0800

    init

--- src/apollo/links/auth.js
+++ src/apollo/links/auth.js
@@ -4,7 +4,7 @@ import { tokenName } from 'configs/app';
 // 添加一个带token的中间件，如果有token就会带上
 const authLink = setContext((_, { headers }) => {
   // const token = localStorage.getItem(tokenName);
-  const token = '51ee154f1a434663bb261470308cf6b0a57e9452';
+  const token = 'your github token';
   return {
     headers: {
       ...headers,
